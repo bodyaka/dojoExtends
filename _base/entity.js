1fda595249ca9d793a2dd8c1d23cc0cef32bbf96
Bogdan Kyrychenko <bogdan50e@gmail.com>
2015-02-04 16:01:50 +0200

    entitu module fix

--- _base/entity.js
+++ _base/entity.js
@@ -4,17 +4,14 @@ define([
 	
 	"dojoExtends/_base/ajax"
 ], function(declare, JsonRest, ajax){
-	
-	var entity = {
+	return declare(null, {
 		constructor: function(args){
 			var target = dojo.config.apiUrlPath;
 			if(args && args.target) target += '/' + args.target;
 			
-			entity.rest = new JsonRest({target: target});
+			this.rest = new JsonRest({target: target});
 	    },
 	
 		rest: null
-	};
-	
-	return declare(null, entity);
+	});
 });
